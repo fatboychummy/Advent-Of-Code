86133aff361c06b7e50aa9706f2891c921a68625
Matthew W <fatboychummy@gmail.com>
2022-12-05 23:44:02 -0700

    Day 6 challenges.

---
+++ 2022/Day6/1/run.lua
@@ -0,0 +1,26 @@
+return function(input, output)
+  local line = input.lines()[1]
+
+  -- brute-force-ish method. sliding window, comparing values
+  -- thanks lua for easy hashmap testing
+  for i = 1, #line - 3 do
+    local substring = line:sub(i, i + 3)
+    local comparator = {}
+    local found = true
+
+    for char in substring:gmatch(".") do
+      if comparator[char] then
+        found = false
+        break
+      end
+      comparator[char] = true
+    end
+
+    if found then
+      output.write(i + 3)
+      return
+    end
+  end
+
+  output.write("Something failed, probably your code, idiot.")
+end
