ffe1a323734247ab06205b2a211f9ef0b22dcaa5
Matthew W <fatboychummy@gmail.com>
2022-12-07 23:39:20 -0700

    Day 8 | Part 2 | Attempt 2

--- 2022/Day8/2/run.lua
+++ 2022/Day8/2/run.lua
@@ -98,12 +98,12 @@ return function(input, output)
   -- ignore edges, their score will always be 0.
   for y = 2, tree_grid.h - 1 do
     for x = 2, tree_grid.w - 1 do
-      local height = tree_grid:Get(y, x)
+      local height = tree_grid:Get(y, x).value
 
       --vertical up
       local s_up = 0
       for _y = y - 1, 1, -1 do
-        local gh = tree_grid:Get(_y, x)
+        local gh = tree_grid:Get(_y, x).value
         s_up = s_up + 1
         if gh >= height then
           break
@@ -113,7 +113,7 @@ return function(input, output)
       --vertical down
       local s_down = 0
       for _y = y + 1, tree_grid.h do
-        local gh = tree_grid:Get(_y, x)
+        local gh = tree_grid:Get(_y, x).value
         s_down = s_down + 1
         if gh >= height then
           break
@@ -123,7 +123,7 @@ return function(input, output)
       --horizontal right
       local s_right = 0
       for _x = x + 1, tree_grid.w do
-        local gh = tree_grid:Get(_y, x)
+        local gh = tree_grid:Get(_y, x).value
         s_right = s_right + 1
         if gh >= height then
           break
@@ -133,13 +133,14 @@ return function(input, output)
       --horizontal left
       local s_left = 0
       for _x = x - 1, 1, -1 do
-        local gh = tree_grid:Get(_y, x)
+        local gh = tree_grid:Get(_y, x).value
         s_left = s_left + 1
         if gh >= height then
           break
         end
       end
 
+      print(s_up, s_down, s_left, s_right)
       -- combine score.
       test(s_up * s_down * s_left * s_right)
     end
