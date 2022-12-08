b04a78cfa4c75de7a6c5490689d3ef131e35dc7b
Matthew W <fatboychummy@gmail.com>
2022-12-07 23:05:06 -0700

    forgor :skull:

--- typings.lua
+++ typings.lua
@@ -3,3 +3,6 @@
 ---@alias fs_type
 ---| 'dir'
 ---| 'file'
+
+---@alias grid {[integer]:{[integer]:grid_object}}
+---@alias grid_object {left:grid_object?,right:grid_object?,up:grid_object?,down:grid_object?,value:any}
