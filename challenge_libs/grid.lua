01fa374cab379ba86835f7f7aea7fc6091f16354
Matthew W <fatboychummy@gmail.com>
2022-12-11 01:45:31 -0700

    Day 9 | Part 2 | Attempt 1

--- challenge_libs/grid.lua
+++ challenge_libs/grid.lua
@@ -1,34 +1,42 @@
-return function()
+--- Basic grid object with Insert and Get capabilities
+---@param initial any The initial value to set if Get called before Insert
+---@return grid grid The grid object.
+return function(initial)
   return {
     w = 0,
     h = 0,
+    nw = 0,
+    nh = 0,
     --- Insert a new value at position y, x
     ---@param self grid The grid.
     ---@param y integer The Y position.
     ---@param x integer The X position.
     ---@param v any The value to insert.
+    ---@return grid_object grid_object The grid object.
     Insert = function(self, y, x, v)
-      if not v then error("bruh", 2) end
       if not self[y] then
         self[y] = {}
         self.h = math.max(y, self.h)
+        self.nh = math.min(y, self.nh)
       end
-      local grid_obj = { value = v }
-
-      grid_obj.up, grid_obj.down, grid_obj.left, grid_obj.right = self:Get(y - 1, x), self:Get(y + 1, x),
-          self:Get(y, x - 1), self:Get(y, x + 1)
+      local grid_obj = { value = v or initial }
 
       self[y][x] = grid_obj
       self.w = math.max(self.w, x)
+      self.nw = math.min(self.nw, x)
+
+      return grid_obj
     end,
 
     --- Get the grid object at position y, x
     ---@param self grid The grid.
     ---@param y integer The Y position.
     ---@param x integer The X position.
-    ---@return grid_object? grid_object The grid object or nil.
+    ---@return grid_object grid_object The grid object.
     Get = function(self, y, x)
-      if self[y] then return self[y][x] end
+      if self[y] and self[y][x] then return self[y][x] end
+
+      return self:Insert(y, x)
     end
   }
 end
--- challenge_libs/linked_grid.lua
+++ challenge_libs/linked_grid.lua
@@ -2,16 +2,18 @@ local function grid()
   return {
     w = 0,
     h = 0,
+    nw = 0,
+    nh = 0,
     --- Insert a new value at position y, x
     ---@param self linked_grid The grid.
     ---@param y integer The Y position.
     ---@param x integer The X position.
     ---@param v any The value to insert.
     Insert = function(self, y, x, v)
-      if not v then error("bruh", 2) end
       if not self[y] then
         self[y] = {}
         self.h = math.max(y, self.h)
+        self.nh = math.min(y, self.nh)
       end
       local grid_obj = { value = v }
 
@@ -20,6 +22,7 @@ local function grid()
 
       self[y][x] = grid_obj
       self.w = math.max(self.w, x)
+      self.nw = math.min(self.nw, x)
     end,
 
     --- Get the grid object at position y, x
