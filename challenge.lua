dbe18a0c4b21490475e65ab8fd1c1fbb78b5377a
Matthew W <fatboychummy@gmail.com>
2023-12-03 02:59:05 -0700

    Generate the input.txt file last This allows all other files to be generated if no `.session_cookie` file exists (or the session cookie is no longer valid).

--- challenge.lua
+++ challenge.lua
@@ -72,9 +72,9 @@ if setup == true then
   logger.info("Setting up challenge.")
   local files = {
     { fs.combine(dir, "README.md"), blank_file },
-    { fs.combine(dir, "input.txt"), input_txt },
     { fs.combine(dir, "run.lua"), run_lua },
     { fs.combine(dir, "test_input.txt"), blank_file },
+    { fs.combine(dir, "input.txt"), input_txt },
   }
 
   logger.info("Make directory: %s", dir)
