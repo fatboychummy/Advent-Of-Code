8bc10c994600f40b89cbe7b3554eb2b1b4d8ca2a
Matthew W <fatboychummy@gmail.com>
2023-12-03 21:48:06 -0700

    Fix installation

--- challenge.lua
+++ challenge.lua
@@ -1,6 +1,58 @@
 -- This code is a hot mess that I originally wrote two years ago and am just sort of updating as I go.
 
-local logger = require "libs.logger"
+-- Quick and small logging library
+
+local logger = {}
+
+local function output_colored(levelname, color, color2, text, ...)
+  local args = table.pack(...)
+  if args.n > 0 then
+    text = string.format(text, ...)
+  end
+
+  term.setTextColor(colors.white)
+  term.write("[")
+  term.setTextColor(color)
+  term.write(levelname)
+  term.setTextColor(colors.white)
+  term.write("] ")
+  term.setTextColor(color2)
+  print(text)
+end
+
+local function output(levelname, color, text, ...)
+  local args = table.pack(...)
+  if args.n > 0 then
+    text = string.format(text, ...)
+  end
+
+  term.setTextColor(colors.white)
+  term.write("[")
+  term.setTextColor(color)
+  term.write(levelname)
+  term.setTextColor(colors.white)
+  print("]", text)
+end
+
+function logger.info(...)
+  output("INFO", colors.white, ...)
+end
+
+function logger.warn(...)
+  output("WARN", colors.orange, ...)
+end
+
+function logger.error(...)
+  output("ERROR", colors.red, ...)
+end
+
+function logger.debug(...)
+  output_colored("DEBUG", colors.lightGray, colors.lightGray, ...)
+end
+
+function logger.custom(levelname, color, color2, ...)
+  output_colored(levelname, color, color2, ...)
+end
 
 package.path = ("%s;/%s/challenge_libs/?.lua;/%s/challenge_libs/?/init.lua")
     :format(package.path, shell.dir(), shell.dir())
