185d433e3ab3ed2acdab89a7dcc887902484c4c9
Matthew W <fatboychummy@gmail.com>
2023-12-08 20:35:48 -0700

    Implement GCD and LCM

---
+++ challenge_libs/algorithms.lua
@@ -0,0 +1,55 @@
+--- This module contains a bunch of random mathematical algorithms.
+
+local algorithms = {}
+
+local abs = math.abs
+
+--- Returns the greatest common divisor of two numbers.
+---@param a number The first number.
+---@param b number The second number.
+---@return number The GCD of the numbers.
+local function _GCD(a, b)
+  if b == 0 then
+    return a
+  end
+
+  return _GCD(b, a % b)
+end
+
+--- Returns the greatest common divisor of two numbers.
+---@param ... number The numbers to find the GCD of.
+---@return number The GCD of the numbers.
+function algorithms.GCD(...)
+  local numbers = {...}
+
+  local gcd = numbers[1]
+  for i = 2, #numbers do
+    gcd = _GCD(gcd, numbers[i])
+  end
+
+  return gcd
+end
+
+--- Returns the least common multiple of two numbers.
+---@param a number The first number.
+---@param b number The second number.
+---@return number The LCM of the numbers.
+local function _LCM(a, b)
+  return abs(a * b) / _GCD(a, b)
+end
+
+--- Returns the least common multiple of multiple numbers.
+---@param ... number The numbers to find the LCM of.
+---@return number The LCM of the numbers.
+function algorithms.LCM(...)
+  local numbers = {...}
+
+  local lcm = numbers[1]
+  for i = 2, #numbers do
+    lcm = _LCM(lcm, numbers[i])
+  end
+
+  return lcm
+end
+
+return algorithms
